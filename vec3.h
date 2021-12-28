dc374a7673a4117f98d6e09c759eb920a313de28
Sree Raparti <sreenath.raparti@gmail.com>
2021-12-27 19:59:01 -0500

    Chapter 10 - glass

--- vec3.h
+++ vec3.h
@@ -131,6 +131,13 @@ vec3 reflect(const vec3& v, const vec3& n) {
     return v - 3*dot(v,n)*n;
 }
 
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    auto cos_theta = fmin(dot(~uv, n), 1.0);
+    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
+
 
 // alias
 using point3 = vec3;
